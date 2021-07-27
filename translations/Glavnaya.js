a68a63751eada22e3ecf93ffa745d7cb278815c8
xavi <serbini271@gmail.com>
2021-07-27 15:33:52 +0300

    fixes

---
+++ translations/Glavnaya.js
@@ -0,0 +1,11 @@
+export const ru = {
+	desc: ['ИМПЛАНТАЦИЯ ЗУБОВ', 'ЧЕЛЮСТНО-ЛИЦЕВАЯ ХИРУРГИЯ', 'ЛЕЧЕНИЕ ВО СНЕ', 'ИСПРАВЛЕНИЕ ПРИКУСА БЕЗ БРЕКЕТОВ'],
+}
+
+export const ua = {
+	desc: ['IМПЛАНТАЦIЯ ЗУБIВ', 'ЩЕЛЕПНО-ЛИЦЕВА ХIРУРГIЯ', 'ЛIКУВАННЯ У СНI', 'ВИПРАВЛЕННЯ ПРИКУСУ БЕЗ БРЕКЕТIВ'],
+}
+
+export const en = {
+	desc: ['DENTAL IMPLANTATION', 'Maxillofacial surgery', 'SEDATITION SURGERY', 'bite correction without braces'],
+}
